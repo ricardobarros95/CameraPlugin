e9847b28f7e09c2a5c796d4fa32685a405bbe162
ricardobarros95 <ricardobarros95@gmail.com>
2016-11-04 11:11:14 +0000

    Commit

---
+++ Assets/Editor/CreateHolder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CreateHolder
+{
+    [MenuItem ("Native Camera/Create Holder")]
+    private static void CreateHolderObject()
+    {
+        CameraPluginn holder = GameObject.FindObjectOfType<CameraPluginn>();
+        if(holder == null)
+        {
+            holder = new GameObject().AddComponent<CameraPluginn>();
+            holder.gameObject.name = "holder";
+        }
+        else
+        {
+            return;
+        }
+    }
+
+
+}
--- Assets/Plugins/CameraPluginn.cs
+++ Assets/Plugins/CameraPluginn.cs
@@ -2,38 +2,48 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.IO;
+using System;
 
+[RequireComponent(typeof(Image))]
 public class CameraPluginn : MonoBehaviour
 {
 
     public Text t;
     public Image Picture { get; set; }
+    public bool PictureReady { get; set; }
 
     public static void StartCamera()
     {
+        Debug.Log("Start Camera: " + DateTime.Now.ToString());
         AndroidJavaClass cameraActivity = new AndroidJavaClass("cameraplugin.blacktorchstudios.camera_plugin.CameraActivity");
         cameraActivity.CallStatic("startCameraActivity");
     }
 
-
     public void DealWithPhoto(string message)
     {
+        Debug.Log("OnActivityResult: " + DateTime.Now.ToString());
         if (System.IO.File.Exists(message))
         {
             byte[] data = System.IO.File.ReadAllBytes(message);
+            Debug.Log("Read all bytes: " + DateTime.Now.ToString());
             Texture2D tex = new Texture2D(1, 1);
+           
             tex.LoadImage(data);
+            Debug.Log("Load Image: " + DateTime.Now.ToString());
 
             Texture2D target = new Texture2D(tex.height, tex.width, tex.format, false);
-
+            
             Color32[] pixels = tex.GetPixels32(0);
             pixels = rotateTextureGrid(pixels, tex.width, tex.height);
             target.SetPixels32(pixels);
             target.Apply();
+            Debug.Log("Rotate Picture: " + DateTime.Now.ToString());
 
             Sprite s = Sprite.Create(target, new Rect(0, 0, target.width, target.height), new Vector2(0.5f, 0.5f));
-
+            Picture = GetComponent<Image>();
             Picture.sprite = s;
+            PictureReady = true;
+            Debug.Log("Sprite Created: " + DateTime.Now.ToString());
         }
         else
         {
